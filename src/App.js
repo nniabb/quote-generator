1fa44f53971ab1281a2335d462ffe42eb3fc1219
nniabb <niabilanishvili17@gmail.com>
2024-04-16 13:01:06 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import axios from 'axios';
 
-class App extends React.Component{
-    state = {advice: ''};
+const App = () => {
+    const [advice, setAdvice] = useState('');
 
-    componentDidMount() {
-        this.fetchQuote()
-    }
+    useEffect(() => {
+        fetchQuote();
+    }, []);
 
-    fetchQuote = () => {
+    const fetchQuote = () => {
         axios.get('https://api.adviceslip.com/advice')
-        .then((response) => {
-            const{advice} = response.data.slip;
-            this.setState({advice})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+            .then((response) => {
+                const { advice } = response.data.slip;
+                setAdvice(advice);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-    render(){
-        const {advice} = this.state;
-
-        return(
-            <div className='app'>
-                <div className='card'>
-                    <h1 className='heading'>{advice}</h1>
-                    <button className='button' onClick={this.fetchQuote}>More Advice</button>
-                </div>
+    return (
+        <div className='app'>
+            <div className='card'>
+                <h1 className='heading'>{advice}</h1>
+                <button className='button' onClick={fetchQuote}>More Advice</button>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default App;
